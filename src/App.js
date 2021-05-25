25440c770a0ac26a5c2a20d4519b042550a49298
Sanjar Kodirov <kodirov.sanzhar@bk.ru>
2021-05-25 23:29:25 +0500

    Router setup

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  Private
+} from './pages'
+function App() {
+  return (
+  <Router>
+    <Navbar />
+    <Sidebar />
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/cart">
+        <Cart />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/products:id" 
+        children={<SingleProduct />} 
+      />
+      <Route path ="/checkout">
+        <Checkout />
+      </Route>
+      <Route path ="*">
+        <Error />
+      </Route>
+    </Switch>
+    <Footer />
+  </Router>
+  )
+}
+
+export default App
